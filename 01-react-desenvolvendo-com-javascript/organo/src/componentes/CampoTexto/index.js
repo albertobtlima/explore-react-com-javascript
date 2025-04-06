a7199742408656552ef33ca568ca930a008feba1
Alberto Lima <alberto.btlima@outlook.com>
2025-04-06 19:13:33 -0300

    up

--- 01-react-desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.js
+++ 01-react-desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  //let valor = "";
+
+  const [valor, setValor] = useState("");
+
+  const aoDigitado = (evento) => {
+    setValor(evento.target.value);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder} required={props.required} />
+      <input
+        value={valor}
+        onChance={aoDigitado}
+        placeholder={props.placeholder}
+        required={props.required}
+      />
     </div>
   );
 };
