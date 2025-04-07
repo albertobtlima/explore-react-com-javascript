8e6eba8019c328a87f1592003c517ed4d38e42dc
Alberto Lima <alberto.btlima@outlook.com>
2025-04-07 15:31:59 -0300

    add: stylos componente Colaborador

---
+++ 01-react-desenvolvendo-com-javascript/organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,21 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img
+          src="https://github.com/albertobtlima.png"
+          alt="Imagem de perfil"
+        />
+      </div>
+
+      <div className="rodape">
+        <h4>Alberto</h4>
+        <h5>Engenheiro de Software</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
