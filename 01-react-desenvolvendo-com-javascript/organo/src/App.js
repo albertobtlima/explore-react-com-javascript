63b9ee4ad0df9d42e52be46547f4bf5a55225082
Alberto Lima <alberto.btlima@outlook.com>
2025-04-09 17:12:49 -0300

    finalizado

--- 01-react-desenvolvendo-com-javascript/organo/src/App.js
+++ 01-react-desenvolvendo-com-javascript/organo/src/App.js
@@ -45,6 +45,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
+    debugger;
     setColaboradores([...colaboradores, colaborador]);
   };
 
