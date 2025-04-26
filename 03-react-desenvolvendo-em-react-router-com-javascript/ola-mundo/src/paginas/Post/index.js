d4559f61fa5ce7fdea868175e7e3a67d23ef0dc4
Alberto Lima <alberto.btlima@outlook.com>
2025-04-26 12:47:07 -0300

    add: componente NaoEncontrada

--- 03-react-desenvolvendo-em-react-router-com-javascript/ola-mundo/src/paginas/Post/index.js
+++ 03-react-desenvolvendo-em-react-router-com-javascript/ola-mundo/src/paginas/Post/index.js
@@ -1,7 +1,28 @@
+import "./Post.css";
 import { useParams } from "react-router-dom";
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
+import posts from "json/posts.json";
 
 export default function Post() {
   const parametros = useParams();
 
-  return "Post...";
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
+
+  if (!post) {
+    return <h1>Post não encontrado...</h1>;
+  }
+
+  return (
+    <PostModelo
+      fotoCapa={`/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>{post.texto}</ReactMarkdown>
+      </div>
+    </PostModelo>
+  );
 }
