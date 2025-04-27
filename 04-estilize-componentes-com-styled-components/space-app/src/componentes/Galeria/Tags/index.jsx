6bc6c16d72e9eac907e398684177267bd50905c8
Alberto Lima <alberto.btlima@outlook.com>
2025-04-27 16:58:59 -0300

    add: estilos pata Tags

--- 04-estilize-componentes-com-styled-components/space-app/src/componentes/Galeria/Tags/index.jsx
+++ 04-estilize-componentes-com-styled-components/space-app/src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,36 @@
+import styled from "styled-components";
+import tags from "./tags.json";
+
+const TagTitulo = styled.h3`
+  color: #d9d9d9;
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Tag = styled.button`
+  font-size: 24px;
+  color: #ffffff;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 12px;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  &:hover {
+    border-color: #c98cf1;
+  }
+`;
+
 const Tags = () => {
-  return <p>Tags</p>;
+  return (
+    <>
+      <TagTitulo>Busque por tags:</TagTitulo>
+      {tags.map((tag) => (
+        <Tag key={tag.id}>{tag.titulo}</Tag>
+      ))}
+    </>
+  );
 };
 
 export default Tags;
