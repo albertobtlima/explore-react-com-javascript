fccabe76ded4fe911c8d07c5a22816e650c83bf7
Alberto Lima <alberto.btlima@outlook.com>
2025-05-12 11:11:09 -0300

    add: useContext

--- 05-gerencie-estados-globalmente-com-contextAPI/meteora/src/components/ListaProdutosCarrinho/index.jsx
+++ 05-gerencie-estados-globalmente-com-contextAPI/meteora/src/components/ListaProdutosCarrinho/index.jsx
@@ -1,14 +1,8 @@
-import React from "react";
 import ItemCarrinhoSuspenso from "@/components/CarrinhoSuspenso/ItemCarrinhoSuspenso";
 import ItemCarrinho from "@/components/ItemCarrinho";
 import { useLocation } from "react-router-dom";
 
-const ListaProdutosCarrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-}) => {
+const ListaProdutosCarrinho = ({ carrinho }) => {
   const location = useLocation();
   return (
     <ul className="list-unstyled">
@@ -17,20 +11,12 @@ const ListaProdutosCarrinho = ({
       ) : (
         carrinho.map((itemCarrinho) => {
           return location.pathname === "/carrinho" ? (
-            <ItemCarrinho
-              key={itemCarrinho.id}
-              itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
-            />
+            <ItemCarrinho key={itemCarrinho.id} itemCarrinho={itemCarrinho} />
           ) : (
             <ItemCarrinhoSuspenso
               key={itemCarrinho.id}
               itemCarrinho={itemCarrinho}
-              adicionarProduto={adicionarProduto}
-              removerProduto={removerProduto}
-              removerProdutoCarrinho={removerProdutoCarrinho}
+              a
             />
           );
         })
