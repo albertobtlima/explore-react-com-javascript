057abb2d9bf9a2ae3f01e2d6840d06205bdf257d
Alberto Lima <alberto.btlima@outlook.com>
2025-06-04 18:40:09 -0300

    add: componente Card

--- 06-praticando-react-com-js/cinetag/src/pages/inicio/index.js
+++ 06-praticando-react-com-js/cinetag/src/pages/inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -11,6 +12,7 @@ function Inicio() {
       <Titulo>
         <h1>Uma lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card id="1" titulo="Gato fofinho" capa="" />
       <Rodape />
     </>
   );
