2fb98d88acc98cc126b339be6e4b5d36334b3d4a
Alberto Lima <alberto.btlima@outlook.com>
2025-06-11 16:30:21 -0300

    add: videos

--- 06-praticando-react-com-js/cinetag/src/pages/inicio/index.js
+++ 06-praticando-react-com-js/cinetag/src/pages/inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
 
 function Inicio() {
   return (
@@ -12,7 +14,11 @@ function Inicio() {
       <Titulo>
         <h1>Uma lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      <Card id="1" titulo="Gato fofinho" capa="" />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
       <Rodape />
     </>
   );
