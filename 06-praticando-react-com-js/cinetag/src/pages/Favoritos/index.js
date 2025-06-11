b531ea8ded38039b3193409d464e125e52349080
Alberto Lima <alberto.btlima@outlook.com>
2025-06-11 17:21:56 -0300

    add: favoritos

--- 06-praticando-react-com-js/cinetag/src/pages/Favoritos/index.js
+++ 06-praticando-react-com-js/cinetag/src/pages/Favoritos/index.js
@@ -1,7 +1,20 @@
+import Banner from "components/Banner";
 import styles from "./Favoritos.module.css";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
 
 function Favoritos() {
-  return;
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card id="1" titulo="Gato" capa="abc" />
+      </section>
+    </>
+  );
 }
 
 export default Favoritos;
